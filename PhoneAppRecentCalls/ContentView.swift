b54fcc4f68fbe7574f73f31fb0c21079ef9add6c
AJ Cooper <acooper@lcs.on.ca>
2023-11-11 19:35:17 -0500

    Initial Commit

---
+++ PhoneAppRecentCalls/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhoneAppRecentCalls
+//
+//  Created by  Adam-James  Cooper on 2023-11-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhoneAppRecentCalls/PhoneAppRecentCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneAppRecentCallsApp.swift
+//  PhoneAppRecentCalls
+//
+//  Created by  Adam-James  Cooper on 2023-11-11.
+//
+
+import SwiftUI
+
+@main
+struct PhoneAppRecentCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
