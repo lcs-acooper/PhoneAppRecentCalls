55486551c343e9cf0ff8fe7fdf6d5f0df0169bd2
AJ Cooper <acooper@lcs.on.ca>
2023-11-11 20:39:36 -0500

    Finished a struct code for the different people in the caller favourate tab

---
+++ PhoneAppRecentCalls/Preview Content/CallerFavorite.swift
@@ -0,0 +1,18 @@
+//
+//  CallerFavorite.swift
+//  PhoneAppRecentCalls
+//
+//  Created by  Adam-James  Cooper on 2023-11-11.
+//
+
+import SwiftUI
+
+struct CallerFavorite: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CallerFavorite()
+}
