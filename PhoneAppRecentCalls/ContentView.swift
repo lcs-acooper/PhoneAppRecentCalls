666a6e0399469386cd6a37e253f755a30877d1ea
AJ Cooper <acooper@lcs.on.ca>
2023-11-20 21:00:59 -0500

    Nothing changed just pushing work

--- PhoneAppRecentCalls/ContentView.swift
+++ PhoneAppRecentCalls/ContentView.swift
@@ -37,6 +37,7 @@ struct ContentView: View {
             }
             //Type of list
             .listStyle(.plain)
+            
             .border(.red, width: 4)
          
             Rectangle()
